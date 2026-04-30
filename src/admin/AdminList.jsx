180e9e08f5f88b07cfc587ad5f8df4f95f4e273b
qwidzit <grisha.yaremko@gmail.com>
2026-04-30 21:14:09 +0800

    Update AdminList.jsx

--- src/admin/AdminList.jsx
+++ src/admin/AdminList.jsx
@@ -1,6 +1,4 @@
-﻿import { useState, useEffect, useRef } from 'react'
-
-import { useState, useEffect, useRef, useCallback } from 'react'
+﻿import { useState, useEffect, useRef, useCallback } from 'react'
 
 let _idSeq = 0
 function genId() {
