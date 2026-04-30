8d1e9a81d4c3e09d03a57fcbfc4b703c027b70dc
qwidzit <grisha.yaremko@gmail.com>
2026-05-01 00:32:26 +0800

    Update xp.js

--- src/utils/xp.js
+++ src/utils/xp.js
@@ -1,3 +1,6 @@
 ﻿export function xpFor(rank) {
-    return Math.max(1, Math.round(1000 / rank))
+    
+    const a = 1.262
+    const b = 0.9941
+    return Math.max(1, Math.round(50*(a*(b**(rank+50))+1)))
 }
